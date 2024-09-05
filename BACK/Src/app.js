ffe0934c3df813a73616d18e510061b9425a213f
Ramiro Heredia <94948260+Gugol-Ram@users.noreply.github.com>
2024-09-05 17:34:55 -0300

    app update with paypal

--- BACK/Src/app.js
+++ BACK/Src/app.js
@@ -2,13 +2,25 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const paymentRouterMP = require("./Routes/routesMP");
+const paymentRouterPP = require("./Routes/routesPP");
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.use("/mp-Payment", paymentRouterMP);
+app.use("/pp-Payment", paymentRouterPP);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "server listening" });
