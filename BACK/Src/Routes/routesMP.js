be6d7b48f22cee98098befedc1329d8561b9a95a
Ramiro Heredia <94948260+Gugol-Ram@users.noreply.github.com>
2024-09-04 22:43:18 -0300

    payment route mp

---
+++ BACK/Src/Routes/routesMP.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const createOrder = require("../Controllers/MP/createOrderMP");
+const paymentRouter = express.Router();
+
+// Orden de pago(para redirigir a la pagina de ML a pagar)
+paymentRouter.post("/mp-order", createOrder);
+
+// Segun la respuesta obtenida de MP:
+paymentRouter.get("/success", (req, res) => {
+  console.log("MercadoPago data: ", req.query);
+  //deberia agregar logica para guardar codigo de transaccion en una BD; y logica para modificar el stock del producto si hubo exito en el pago
+  res.redirect("http://localhost:5173/"); //redirijo a la ruta que quiero mostrar luego de que MP me devuelve el control, como no tengo otras lo redirijo a home, pero podria ser la que quiera/necesaria
+
+  // res.status(200).json({ message: "Pago Aprobado" });Mensaje de prueba para mostrar "algo" mientras no tenia a donde redirigir
+});
+paymentRouter.get("/failure", (req, res) => {
+  console.log("Mercado Pago Data: ", req.query);
+
+  res.redirect("http://localhost:5173/");
+
+  // res.status(200).json({ message: "Pago rechazado" });Mensaje de prueba para mostrar "algo" mientras no tenia a donde redirigir
+
+  paymentRouter.get("/peding", (req, res) => {
+    console.log("Mercado Pago DAta: ", req.query);
+
+    res.redirect("http://localhost:5173/");
+
+    // res.status(200).json({ message: "Pendiente" });Mensaje de prueba para mostrar "algo" mientras no tenia a donde redirigir
+  });
+});
