f0bfd4654dca5530c35ab05f505472e468301d32
Ramiro Heredia <94948260+Gugol-Ram@users.noreply.github.com>
2024-09-13 21:58:53 -0300

    success&failedRoutes

--- BACK/Src/Routes/routesPP.js
+++ BACK/Src/Routes/routesPP.js
@@ -8,4 +8,10 @@ const paymentRouterPP = express.Router();
 paymentRouterPP.post("/create-order", createPayPalOrder);
 paymentRouterPP.post("/capture-order/:orderID", capturePayPalOrder);
 
+paymentRouterPP.get("/success", (req, res) => {
+  console.log("PayPal data: ", req.query);
+  // res.status(200).json({ message: "Payment Aproved" });
+  res.redirect("http://localhost:5173/");
+});
+
 module.exports = paymentRouterPP;
