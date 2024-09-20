25748d85585676fb092b9da2303fbd1dc960bc75
Ramiro Heredia <94948260+Gugol-Ram@users.noreply.github.com>
2024-09-19 23:33:37 -0300

    Routes update

--- FRONT/src/main.jsx
+++ FRONT/src/main.jsx
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import SuccessPage from "./Utils/PayPal/SuccessPage.jsx";
+import PayPalSuccessPay from "./Utils/PayPal/SuccessPage.jsx";
+import PayPalFailurePay from "./Utils/PayPal/FailurePage.jsx";
+import PayPalUnknowPay from "./Utils/PayPal/UnknowStatusPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/success" element={<SuccessPage />} />
+        <Route path="/pp-pay-success" element={<PayPalSuccessPay />} />
+        <Route path="/pp-pay-failure" element={<PayPalFailurePay />} />
+        <Route path="/pp-pay-unknow" element={<PayPalUnknowPay />} />
       </Routes>
     </Router>
   </React.StrictMode>
